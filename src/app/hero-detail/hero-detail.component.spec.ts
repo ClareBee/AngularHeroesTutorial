de8ea5f4ee2be1ba16b80c9060bc977d4b02aaaa
Clare Blackburne <chblackburne@gmail.com>
2020-01-13 13:57:06 +0000

    mock hero service in hero-detail component spec

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,16 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs'
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from "@angular/router/testing";
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { Hero } from '../hero';
 
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let httpClientSpy: { get: jasmine.Spy };
+  let heroService: HeroService;
+  let messageService: MessageService;
 
   beforeEach(async(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    messageService = new MessageService();
+    heroService = new HeroService(<any>httpClientSpy, messageService);
+
     TestBed.configureTestingModule({
       imports: [FormsModule, ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
       declarations: [ HeroDetailComponent ]
@@ -29,11 +40,15 @@ describe('HeroDetailComponent', () => {
   });
 
   it('should display hero name', async () => {
-    const hero = { name: 'Bob', id: 1123 }
-    component.hero = hero;
-    await component.getHero(); // get the promise value
-    const compiled = fixture.debugElement.nativeElement;
-    console.log(compiled)
-    expect(compiled.querySelector('.hero-name').textContent).toContain('BOB Details');
+    const expectedHero: Hero = { name: 'Bob', id: 1123 }
+    httpClientSpy.get.and.returnValue(of(expectedHero));
+    heroService.getHero(1123).subscribe(
+      hero => expect(hero).toEqual(expectedHero, 'expected hero'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    // const compiled = fixture.debugElement.nativeElement;
+    // console.log(compiled)
+    // expect(compiled.querySelector('.hero-name').textContent).toContain('BOB Details');
   })
 });
